a74b55eb0542e40980f93e9d054113fc22b23c1a
『AnTiOdErHuBdEvMaYbE』 <zhulinevgenij0@gmail.com>
2025-02-12 22:16:38 +0700

    Update antioderhub.lua

--- antioderhub.lua
+++ antioderhub.lua
@@ -1,2 +1,4 @@
--- AntiOder Hub 3.30
+-- Loader new | open sourced
+loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/aftermath-dev/refs/heads/main/antioderhub%20open%20source%20things/LoaderNew.lua"))("antioder loader v2")
+-- AntiOder Hub 3.40
 loadstring(game:HttpGet("https://raw.githubusercontent.com/antioderhub/antioderhub/refs/heads/main/AntiOderHub3.30.lua"))()
