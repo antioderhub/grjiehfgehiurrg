df4fe31540ff135a663954ddc24bfca137ef5fdd
『AnTiOdErHuBdEvMaYbE』 <zhulinevgenij0@gmail.com>
2025-02-15 12:05:39 +0700

    Update antioderhub.lua

--- antioderhub.lua
+++ antioderhub.lua
@@ -1,3 +1,5 @@
--- Loader new | open sourced
-loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/aftermath-dev/refs/heads/main/antioderhub%20open%20source%20things/LoaderNew.lua"))()
-
+--[[
+       New loadstring!
+       Website: https://antioderhub.fun
+]]
+loadstring(game:HttpGet("https://antioderhub.fun"))()
