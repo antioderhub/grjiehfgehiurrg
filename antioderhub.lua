a9c7daeed78bcaf9a9c27970fc614881bd75e3a5
『AnTiOdErHuBdEvMaYbE』 <zhulinevgenij0@gmail.com>
2025-02-21 13:11:51 +0700

    Update antioderhub.lua

--- antioderhub.lua
+++ antioderhub.lua
@@ -1,5 +1,5 @@
 --[[
        New loadstring!
-       Website: https://antioderhub.fun
+       New website: https://antioderhub.lol
 ]]
-loadstring(game:HttpGet("https://antioderhub.fun"))()
+loadstring(game:HttpGet("https://antioderhub.lol"))()
